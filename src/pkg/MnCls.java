32045fa90dcf37d3f77f9c2da7931418c7e06bfe
ruth.lennon <ruth.lennon@lyit.ie>
2017-07-10 02:12:31 +0100

    first commit

---
+++ src/pkg/MnCls.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package pkg;
+
+/**
+ * @author Ruth.Lennon
+ *
+ */
+public class MnCls {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
